9b74eacc98355ca8133cd9e920c9b61265d405c6
Vasyl Sokolyk <vsokolyk@gmail.com>
2025-04-12 15:11:31 +0300

    Update build.ts

--- build.ts
+++ build.ts
@@ -1,7 +1,9 @@
+import "./src/index.ts";
+
 await Bun.build({
 	entrypoints: ["./src/index.ts"],
 	outdir: "./dist",
-	target: "bun",
-	minify: false,
-	// banner: "#!/usr/bin/env node",
+	target: "node",
+	// minify: false,
+	banner: "#!/usr/bin/env node",
 });
